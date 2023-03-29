16da963889f87071edbd0469a96d1e376c04ac21
AYUSHJAIN5678 <95229954+AYUSHJAIN5678@users.noreply.github.com>
2023-03-29 15:54:31 +0530

    Create Sort.java

---
+++ Sort.java
@@ -0,0 +1,10 @@
+public class Solution{
+  
+  public static void main(String[] args){
+    int[] arr = {9,3,2,4};
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+    
+  }
+
+}
